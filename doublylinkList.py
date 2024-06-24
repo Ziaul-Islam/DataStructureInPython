9cab53f27c6fb64689c92081232c1bbc06ef269d
Ziaul Islam <zia.isl@outlook.com>
2024-06-23 19:18:23 -0500

    day 3 commit 1

--- doublylinkList.py
+++ doublylinkList.py
@@ -18,5 +18,30 @@ class DoublyLinkList:
             print(temp.value)
             temp = temp.next
     
+    def printList_rev(self):
+        temp = self.tail
+        print("Print the link List reverse:")
+        while temp is not None:
+            print(temp.value)
+            temp = temp.prev
+
+    def append(self, value):
+        new_Node = Node(value)
+        if self.head == None and self.tail == None:
+            self.head = new_Node
+            self.tail = new_Node
+        else:
+            self.tail.next = new_Node
+            new_Node.prev = self.tail
+            self.tail = new_Node
+        self.length += 1
+
+
+        
+    
 myDoublyLinlList = DoublyLinkList(7)
+myDoublyLinlList.append(23)
+myDoublyLinlList.append(1)
+myDoublyLinlList.append(4)
 myDoublyLinlList.printList()
+myDoublyLinlList.printList_rev()
