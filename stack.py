e512ad7340024a37fd4643fc91d5e3e9937922d1
Ziaul Islam <zia.isl@outlook.com>
2024-06-27 17:36:02 -0500

    Day 4

---
+++ stack.py
@@ -0,0 +1,44 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class Stack:
+    def __init__(self, value):
+        new_Node = Node(value)
+        self.top = new_Node
+        self.height = 1
+    
+    def printStack(self):
+        temp = self.top
+        while temp != None:
+            print(temp.value," ,", end='')
+            temp = temp.next
+        print()
+    
+    def push(self, value):
+        new_Node = Node(value)
+        new_Node.next = self.top
+        self.top = new_Node
+        self.height += 1
+
+    def pop(self):
+        if self.height == 0:
+            return
+        temp = self.top
+        self.top = self.top.next
+        temp.next = None
+        self.height -= 1
+        return temp.value
+
+myStack = Stack(11)
+myStack.push(7)
+myStack.push(3)
+myStack.printStack()
+print("My Stack Top : ", myStack.top.value)
+print("My Stack pop: ", myStack.pop())
+print("My Stack pop: ", myStack.pop())
+print("My Stack pop: ", myStack.pop())
+print("My Stack pop: ", myStack.pop())
+        
+        
