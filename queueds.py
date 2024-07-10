53c7ce802d00b4016e12704981df78b29c4e3c85
Ziaul Islam <zia.isl@outlook.com>
2024-07-09 23:12:56 -0500

    day 4 second

--- queueds.py
+++ queueds.py
@@ -9,6 +9,7 @@ class Queue:
         new_Node = Node(value)
         self.first = new_Node
         self.last = new_Node
+        self.length = 1
 
     def print(self):
         temp = self.first
@@ -18,5 +19,47 @@ class Queue:
             temp = temp.next
         print("\n")
 
+    def enqueue(self, value):
+        new_Node = Node(value)
+        if self.length == 0:
+            self.first = new_Node
+            self.last = new_Node
+            self.length = 1
+        else:
+            self.last.next = new_Node
+            self.last = new_Node
+            self.length += 1
+    
+    def dequeue(self):
+        if self.length == 0:
+            return None
+        elif self.length == 1:
+            temp = self.first
+            self.first = None
+            self.last = None
+            self.length = 0
+            return temp.value
+        else:
+            temp = self.first
+            self.first = self.first.next
+            self.length -= 1
+            return temp.value
+        
 myQueue = Queue(4)
+myQueue.enqueue(11)
+myQueue.enqueue(7)
+myQueue.enqueue(23)
+myQueue.print()
+print(myQueue.dequeue())
+myQueue.print()
+print(myQueue.dequeue())
+myQueue.print()
+print(myQueue.dequeue())
+myQueue.print()
+print(myQueue.dequeue())
+myQueue.print()
+print(myQueue.dequeue())
+myQueue.print()
+myQueue.enqueue(87)
+myQueue.enqueue(3)
 myQueue.print()
